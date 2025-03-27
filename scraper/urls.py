63f1613d0d003f4e379e0f378c16d4e5f72698e0
vps2 <root@vps2>
2025-03-27 19:55:55 +0000

    🔄 Manuelles Update

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,10 +1,13 @@
 watchlist_urls = {
     # Webseiten & Strukturen
+    "Twitter AfD": "https://nitter.space/AfD",
+    "Twitter AfD Bundestag": "https://nitter.space/AfDimBundestag",
     "Twitter Gerrit Huy": "https://nitter.space/GerritHuy",
     "Twitter Benjamin Nolte": "https://nitter.space/BenjaminNolte3",
     "Twitter Alexander Gauland": "https://nitter.space/ag_afd",
     "Twitter Ingo Hahn": "https://nitter.space/IngoHahnAfD",
     "Twitter Alice Weidel": "https://nitter.space/Alice_Weidel",
     "Twitter Björn Höcke": "https://nitter.space/BjoernHoecke",
-    "Twitter Beatrix von Storch": "https://nitter.space/Beatrix_vStorch"
+    "Twitter Beatrix von Storch": "https://nitter.space/Beatrix_vStorch",
+    "Twitter Tino Chrupalla": "https://nitter.space/Tino_Chrupalla"
 }
