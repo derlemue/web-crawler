53d2507777e13cf2bec0897eed6902a513b0cfde
vps2 <root@vps2>
2025-03-28 21:14:22 +0000

    update v1.9.2

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,5 +1,20 @@
 watchlist_urls = {
     # Webseiten & Strukturen
+    "AfD Weilheim-Schongau": "https://www.afd-weilheim-schongau.de/",
+    "AfD Weilheim-Schongau News": "https://www.afd-weilheim-schongau.de/neu/",
+    "AfD Bayern KV Weilheim-Schongau": "https://afdbayern.de/kreis-ortsverbaende/oberbayern/weilheim-schongau/",
+    "AfD Bayern KV Garmisch-Partenkirchen": "https://afdbayern.de/kreis-ortsverbaende/oberbayern/garmisch-partenkirchen/",
+    "AfD Bayern KV Starnberg": "https://afdbayern.de/kreis-ortsverbaende/oberbayern/starnberg/",
+    "AfD Bayern KV Bad Tölz": "https://afdbayern.de/kreis-ortsverbaende/oberbayern/badtoelz/",
+    "AfD Bayern KV Miesbach": "https://afdbayern.de/kreis-ortsverbaende/oberbayern/oberbayern-sued/",
+    "AfD Oberbayern": "https://www.afd-oberbayern.de/",
+    "AfD Miesbach": "https://www.afd-miesbach.de/",
+    "AfD Miesbach News": "https://www.afd-miesbach.de/corona-fakten-medien-2/",
+    "AfD Ulrich Singer": "https://singer-afd.de/",
+    "AfD Bad Tölz": "https://www.afd-toelz.de/",
+    "AfD Bad Tölz News": "https://www.afd-toelz.de/pressemitteilungen-des-afd-kreisverbandes-toelz/",
+    "AfD Starnberg": "https://afd-starnberg.de/",
+    # Social Media
     "Twitter AfD": "https://nitter.space/AfD",
     "Twitter AfD Bundestag": "https://nitter.space/AfDimBundestag",
     "Twitter Gerrit Huy": "https://nitter.space/GerritHuy",
